4a7d73a736824fe4d24400a75c9acdd35c0f42ea
poicc <2631315464@qq.com>
2022-03-22 15:16:40 +0800

    客户端登录页面

--- uni-article/pages/login/login.vue
+++ uni-article/pages/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-	<view>
+	<view class="h-100 bg-white">
 		<!-- #ifndef MP -->
 		<uni-status-bar></uni-status-bar>
 		<view class="iconfont icon-guanbi flex  align-center justify-start font-lg size-100 p-2" @tap="back()"></view>
@@ -8,51 +8,79 @@
 		<template v-if="choice">
 			<!-- 账号密码登录 -->
 			<view>
-				<view class="text-center" style="padding-top: 90rpx;padding-bottom: 90rpx;"><text class="h3 text-body">账号密码登录</text></view>
-				<view class="flex p-2 mb-1"><input type="text" v-model="phone" placeholder="手机号" class="border-bottom p-2 flex-1" /></view>
-				<view class="flex p-2 mb-1">
+				<view class="text-center" style="padding-top: 130rpx;padding-bottom: 130rpx;">
+					<text class="h3 text-body">账号密码登录</text>
+				</view>
+				<view class="flex p-1 mb-1">
+					<input type="text" v-model="phone" placeholder="手机号" class="border-bottom p-2 flex-1" />
+				</view>
+				<view class="flex p-1 mb-1">
 					<input type="password" v-model="password" placeholder="请输入密码" class="p-2 flex-1" />
 					<view class="font-sm text-muted px-3 flex align-center">忘记密码?</view>
 				</view>
-				<view class="p-2"><button class="rounded-circle bg-pink text-white" @tap="login">登录</button></view>
+				<view class="p-2">
+					<button class="rounded-circle bg-pink text-white" :class="disabled ? 'bg-ponk-disabled' : 'bg-pink'" :loading="loading" @tap="login">{{loading ? '登录中...' : '登录'}}</button>
+				</view>
 			</view>
 
 			<view class="text-center mt-3 text-blue font-sm">
-				<text @tap="choice = !choice">手机验证码登录</text>
-				<text>|</text>
-				<text class="ml-1">登录遇到问题</text>
+				<text @tap="choice = !choice">验证码登录</text>
+				<text class="mx-2">|</text>
+				<text>登录遇到问题</text>
 			</view>
 
-			<view class="flex align-center justify-center text-muted mt-3">
-				注册即代表同意
-				<text class="text-blue">《xxx协议》</text>
-				<!-- <input type="checkbox"/> -->
+			<view class="text-center text-muted mt-3">
+				<label class="flex justify-center ">
+					<checkbox :value="checked" />
+					<view class="">
+						<text>注册即代表同意</text>
+						<text class="text-blue">《xxx协议》</text>
+					</view>
+				</label>
 			</view>
 		</template>
+		
 		<template v-else>
 			<view>
-				<view class="text-center" style="padding-top: 90rpx;padding-bottom: 90rpx;"><text class="h3 text-body">手机验证码登录</text></view>
-				<view class="flex px-2 mb-3 ">
-					<view class="font px-2 flex align-center ">+86</view>
+				<view class="text-center" style="padding-top: 130rpx;padding-bottom: 130rpx;"><text class="h3 text-body">手机验证码登录</text></view>
+				<view class="flex p-1 mb-1 ">
+					<view class="border-right font px-2 flex align-center">+86</view>
 					<input type="text" v-model="phone" placeholder="手机号" class="border-bottom p-2 flex-1" />
 				</view>
-				<view class="flex px-2 mb-3">
-					<input type="number" v-model="verifyCode" placeholder="请输入验证码" class="p-2 flex-1" />
-					<view @tap="getCode" :class="limitTime > 0 ? 'bg-ponk-disabled' : 'bg-pink'" class="font-sm text-white px-3 flex align-center bg-pink rounded">获取验证码</view>
+				<view class="flex p-2 mb-1">
+					<input type="text" v-model="verifyCode" placeholder="请输入验证码" class="p-2 flex-1" />
+					<view @tap="getCode" :class="limitTime > 0 ? 'bg-ponk-disabled' : 'bg-pink'" class="font-sm text-white px-3 flex align-center bg-pink rounded">{{limitTime > 0 ? limitTime + 's' : '获取验证码' }}</view>
 				</view>
-				<view class="p-2"><button class="rounded-circle bg-pink text-white" @tap="login">登录</button></view>
+				<view class="p-2"><button class="rounded-circle bg-pink text-white" @tap="login" :class="disabled ? 'bg-ponk-disabled' : 'bg-pink'">登录</button></view>
 			</view>
 
 			<view class="text-center mt-3 text-blue font-sm">
-				<text @tap="choice = !choice">手机验证码登录</text>
-				<text>|</text>
-				<text class="ml-1">登录遇到问题</text>
+				<text @tap="choice = !choice">账号密码登录</text>
+				<text class="mx-2">|</text>
+				<text>登录遇到问题</text>
 			</view>
 
-			<view class="flex align-center justify-center text-muted mt-3">
-				注册即代表同意
-				<text class="text-blue">《xxx协议》</text>
-				<!-- <input type="checkbox"/> -->
+			<view class="text-center text-muted mt-3">
+				<label class="flex justify-center ">
+					<checkbox :value="checked" />
+					<view class="">
+						<text>注册即代表同意</text>
+						<text class="text-blue">《xxx协议》</text>
+					</view>
+				</label>
+			</view>
+			
+			<!-- 第三方登录 -->
+			<view class="flex align-center px-5 py-3 mt-3">
+				<view class="flex-1 flex align-center justify-center" @click="appLogin">
+					<view class="iconfont icon-weixin bg-success font-lg text-white flex align-center justify-center rounded-circle size-100"></view>
+				</view>
+				<view class="flex-1 flex align-center justify-center">
+					<view class="iconfont icon-QQ bg-primary font-lg text-white flex align-center justify-center rounded-circle size-100"></view>
+				</view>
+				<view class="flex-1 flex align-center justify-center">
+					<view class="iconfont icon-xinlangweibo bg-warning font-lg text-white flex align-center justify-center rounded-circle size-100"></view>
+				</view>
 			</view>
 		</template>
 	</view>
@@ -71,15 +99,16 @@ export default {
 			password: "",
 			verifyCode: "",
 			limitTime: 0,
-			disabled: false
+			disabled: false,
+			checkes: false,
+			loading: false
 		};
 	},
 	methods: {
 		back() {
-			uni.showToast({
-				title: "返回",
-				duration: 2000
-			});
+			uni.navigateBack({
+				delta: 1
+			})
 		},
 		validate() {
 			//手机号正则
@@ -108,7 +137,7 @@ export default {
 			}).then((res) => {
 				console.log(res[1].data.data)
 				uni.showToast({
-					title:res[1].data.data.nickname
+					title: res[1].data.data.nickname
 				})
 			})
 		},
@@ -120,7 +149,7 @@ export default {
 			} else {
 				this.disabled = true
 			}
-			this.limitTime = 20;
+			this.limitTime = 5;
 			let timer = setInterval(() => {
 				if(this.limitTime >= 1) {
 					this.limitTime --
